aa4e73d57807800a04f7b0812888aa80b030d42d
Michael Adair <mjadair91@gmail.com>
2020-03-10 18:21:58 +0000

    added content

---
+++ src/Components/Heading.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+// import Animation from './Animation'
+
+const Heading = () => {
+
+  const [offset, setOffset] = useState(0)
+
+  const parallaxShift = () => {
+    setOffset(window.pageYOffset)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', parallaxShift)
+  }, [offset])
+
+
+  return <>
+
+    <section className="hero is-light is-fullheight" style={{ backgroundPositionY: -offset / 2 }}>
+
+      <div className="hero-body heading">
+        <div className="container">
+          <h1 className="nameTitle text-flicker-in-glow">Michael Adair</h1>
+        </div>
+      </div>
+
+      
+    </section>
+
+  </>
+
+}
+
+
+export default Heading
---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return <div className="hero-foot">
+    <nav className="navbar is-fixed-bottom is-fullwidth">
+      <div className="container">
+
+        <a className="navbar-item" href="#about">about</a>
+        <a className="navbar-item" href="#skills">skills</a>
+        <a className="navbar-item" href="#projects">projects</a>
+        <a className="navbar-item" href="#contact">contact</a>
+
+      </div>
+    </nav>
+  </div>
+
+}
+
+export default Navbar
