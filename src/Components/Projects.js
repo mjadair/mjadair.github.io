28917bdfc727a03db17085fcb75eadf1ca5172a1
Michael Adair <mjadair91@gmail.com>
2020-03-13 21:24:49 +0000

    updated

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSwipeable } from 'react-swipeable'
+
+import TripBitHomeMobile from '../assets/TripBitHomeMobile.png'
+import TripBitHomeDesktop from '../assets/TripBitHomeDesktop.png'
+import TripBitBadgesDesktop from '../assets/TripBitBadgesDesktop.png'
+import TripBitBadges2Desktop from '../assets/TripBitBadges2Desktop.png'
+import TripBitMapDesktop from '../assets/TripBitMapDesktop.png'
+import TripBitGroupsDesktop from '../assets/TripBitGroupsDesktop.png'
+
+
+
+const config = {
+  delta: 10,                             // min distance(px) before a swipe starts
+  preventDefaultTouchmoveEvent: true,   // preventDefault on touchmove, *See Details*
+  trackTouch: true,                      // track touch input
+  trackMouse: false,                     // track mouse input
+  rotationAngle: 0                      // set a rotation angle
+}
+
+
 
 
 const Projects = () => {
 
-  return <section className='hero is-fullheight heading'>
+  return <section
+  className='hero is-fullheight heading'
+>
 
-    <div className="hero-body">
-      <div className="container has-text-centered">
-        <h1 className="title">
-          PROJECTS
-        </h1>
-        <p></p>
-        <br />
-        <p></p>
-      </div>
+  <div className="hero-body">
+    <div className="container has-text-centered">
+      <h1 className="title">
+        Projects
+      </h1>
+      <p></p>
+      <br />
+      <p></p>
     </div>
-  </section>
+  </div>
+</section>
 
 }
 export default Projects
+
+
+
+
+
+
+
+
+
+    
+      
+
