af3060cb9664632f30a838861550c21c9e9dd606
Michael Adair <mjadair91@gmail.com>
2020-04-05 18:46:32 +0100

    contact page and navbar

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,23 +1,41 @@
 import React from 'react'
 
 
-const About = () => {
+const Contact = () => {
 
-  return <section
-    className='hero is-fullheight' id='contact'
-  >
+  return <section className='hero is-fullheight about-body' id="contact">
+    <h1 className="nameTitle hero-head" id="about-title">Contact</h1>
+    <div className="hero-body">
 
-    <div className="hero-body about-body">
       <div className="container">
-        <h1 className="title" >
-          CONTACT
-        </h1>
-        <p></p>
-        <br />
-        <p></p>
+
+        <div className="columns is-mobile is-centered">
+
+
+
+
+
+          <div className="column is-half">
+
+            <h3>Email <i className="fa fa-envelope"></i></h3>
+            <h3>GitHub <i className="devicon-github-plain"></i> </h3>
+            <h3>LinkedIn <i className="fa fa-linkedin-square"></i> </h3>
+
+          </div>
+
+
+
+
+
+
+        </div>
+
       </div>
+
     </div>
-  </section>
+
+  </section >
+
 
 }
-export default About
+export default Contact
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
-import About from './About'
 
 
 const Navbar = () => {
- 
+
 
 
 
   return <nav className="navbar is-fixed-bottom" id="nav-styling">
-    <AnchorLink href="#heading" className="navbar-item">
-      <span className="icon">
-        Home
-      </span>
+    {/* <AnchorLink href="#heading" className="navbar-item">
+      
+    Home
+  
+    </AnchorLink> */}
+    <AnchorLink href="#about" className="navbar-item link">
+      About 
     </AnchorLink>
-    <AnchorLink href="#about" className="navbar-item">
-      About
-    </AnchorLink>
-    <AnchorLink href="#projects" className="navbar-item">
+    <AnchorLink href="#projects" className="navbar-item link">
       Projects
     </AnchorLink>
-    <AnchorLink href="#contact" className="navbar-item">
+    <AnchorLink href="#contact" className="navbar-item link">
       Contact
     </AnchorLink>
   </nav>
