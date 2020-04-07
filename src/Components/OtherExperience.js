012be8dde5f73e23d165bb5dc09eb39608fd48e7
Michael Adair <mjadair91@gmail.com>
2020-04-07 15:43:00 +0100

    updated about

--- src/Components/OtherExperience.js
+++ src/Components/OtherExperience.js
@@ -13,23 +13,25 @@ const OtherExperience = ({ Slide }) => {
           <div className="content">
             <p className="title project-title" id="dev-heading">Other Experience</p>
             <div className="content">
-              <p className="about-text"> </p>
-              <p className="about-text"></p>
-              <p className="about-text"></p>
+              <p className="about-text">I worked at the BBC for 5 years in a variety of roles including Assistant Producer and Production Manager.</p>
+              <p className="about-text">For most of my time with the BBC, I worked on BBC Radio 4&apos;s flagship conversation programme, <a className="gold" href="https://www.bbc.co.uk/programmes/b006qnmr" target="_blank" rel="noopener noreferrer" >Desert Island Discs</a> and was part of the team that won the Broadcasting Press Guild &apos;Best Radio Programme&apos; Award 2017.</p>
+              <p className="about-text">I have also worked as a Production Manager</p>
             </div>
           </div>
         </article>
       </div>
       <div className="tile is-parent is-10 about-tile">
         <article className="tile is-child is-9 notification about-tile">
+        
           <figure className="image michael-image">
-            <img src={DID}/>
+            <img src={DID} />
           </figure>
+          <p className="about-text overline">With Tom Hanks </p>
         </article>
 
       </div>
 
-      </div>
+    </div>
 
 
 
@@ -37,7 +39,7 @@ const OtherExperience = ({ Slide }) => {
 
 
 
-    }
-    
-    
+}
+
+
 export default OtherExperience
