9c805b5307b4e16b7b05d50ca19010f28b23d414
Michael Adair <mjadair91@gmail.com>
2020-04-07 16:22:35 +0100

    added experience

--- src/Components/OtherExperience.js
+++ src/Components/OtherExperience.js
@@ -14,15 +14,16 @@ const OtherExperience = ({ Slide }) => {
             <p className="title project-title" id="dev-heading">Other Experience</p>
             <div className="content">
               <p className="about-text">I worked at the BBC for 5 years in a variety of roles including Assistant Producer and Production Manager.</p>
-              <p className="about-text">For most of my time with the BBC, I worked on BBC Radio 4&apos;s flagship conversation programme, <a className="gold" href="https://www.bbc.co.uk/programmes/b006qnmr" target="_blank" rel="noopener noreferrer" >Desert Island Discs</a> and was part of the team that won the Broadcasting Press Guild &apos;Best Radio Programme&apos; Award 2017.</p>
-              <p className="about-text">I have also worked as a Production Manager</p>
+              <p className="about-text">For most of my time with the BBC, I worked on BBC Radio 4&apos;s flagship conversation programme and one of the UK&apos; most popular podcasts, <a className="gold" href="https://www.bbc.co.uk/programmes/b006qnmr" target="_blank" rel="noopener noreferrer" >Desert Island Discs</a> and was part of the small team that won the Broadcasting Press Guild &apos;Best Radio Programme&apos; Award 2017.</p>
+              <p className="about-text">I have also worked as a Production Manager, planning and running several large scale events for BBC Radio Production, including the BBC Edinburgh Festivals Project and the 2018 Royal Wedding.</p>
+              <p className="about-text">For my full CV have a look at my <a href="https://www.linkedin.com/in/michael-adair1/" target="_blank" rel="noopener noreferrer" className="gold"> LinkedIn </a>. </p>
             </div>
           </div>
         </article>
       </div>
       <div className="tile is-parent is-10 about-tile">
         <article className="tile is-child is-9 notification about-tile">
-        
+
           <figure className="image michael-image">
             <img src={DID} />
           </figure>
